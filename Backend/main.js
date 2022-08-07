43e4f05a089c8b1755fca5d6c77f100f3a7ad603
Vusmonth <39846007+Vusmonth@users.noreply.github.com>
2022-08-07 18:07:10 -0300

    fix: db fixed

--- Backend/main.js
+++ Backend/main.js
@@ -6,33 +6,3 @@ app.listen(port, () => {
   console.log(`\n----> Server listening on port ${port} <----`)
 });
 
-
-/* const con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: "mydb"
-});
-
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-
-  con.query("CREATE DATABASE IF NOT EXISTS mydb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
-
-  con.query("CREATE TABLE IF NOT EXISTS reminders (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), title VARCHAR(255), datetime VARCHAR(255))",
-    function (err, result) {
-      if (err) throw err;
-      console.log("Table created");
-    });
-
-  var sql = "INSERT INTO reminders (name, title, datetime) VALUES ('Company Inc', 'Highway 37', 'teste')";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });
-});
- */
