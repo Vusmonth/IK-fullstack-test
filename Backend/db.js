298563944c6fa436633f408a477574858b4503ed
Vusmonth <39846007+Vusmonth@users.noreply.github.com>
2022-08-09 11:42:01 -0300

    fix: db autoclosing connection

--- Backend/db.js
+++ Backend/db.js
@@ -1,36 +1,11 @@
-const mysql = require('mysql');
-require('dotenv').config()
+const mysql = require("mysql");
+const dbConfig = require("./config/db.config");
 
-// mysql://ba91d16472ca32:62c390bf@us-cdbr-east-06.cleardb.net/heroku_6203d4477bec9ed?reconnect=true
-
-module.exports = con = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+var connection = mysql.createPool({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DATABASE
 });
 
-con.connect(function (err) {
-    if (err) {
-        console.log(err)
-    }
-    console.log("Connected!");
-    
-    //Cria nova base de dados caso não exista assim que o servidor é iniciado
-    con.query("CREATE DATABASE IF NOT EXISTS heroku_6203d4477bec9ed", function (err, result) {
-        if (err) {
-            console.log(err)
-        }
-        console.log("Database created");
-    });
-
-    //Cria nova tabela caso não exista assim que o servidor é iniciado
-    con.query("CREATE TABLE IF NOT EXISTS reminders (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), title VARCHAR(255), datetime VARCHAR(255))",
-        function (err, result) {
-            if (err) {
-                console.log(err)
-            }
-            console.log("Table created");
-        });
-});
-
+module.exports = connection;
